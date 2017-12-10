057b37508bb56abc3f3b750775c52ebe568ecba6
arun <raj.no888@gmail.com>
2017-12-10 11:45:54 +0530

    mysql database

--- StPeace/settings.py
+++ StPeace/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'StPeace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stpeace',
+        'USER': 'root',
+        'PASSWORD': '1234',
+
     }
 }
 
